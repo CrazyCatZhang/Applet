0a068e2a923985a679945c09f314f41c15e4475d
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-09-27 09:48:56 +0800

    Get user information

--- NetEaseMusic/pages/index/index.js
+++ NetEaseMusic/pages/index/index.js
@@ -5,7 +5,7 @@ Page({
      * Page initial data
      */
     data: {
-        msg: 'CatZhang'
+        userInfo: {}
     },
 
     toLogs() {
@@ -19,6 +19,17 @@ Page({
      */
     onLoad(options) {
 
+    },
+
+    getUserProfile(e) {
+        wx.getUserProfile({
+            desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+            success: (res) => {
+                this.setData({
+                    userInfo: res.userInfo
+                })
+            }
+        })
     },
 
     /**
